9056988a64b3d1017ea83b8b3e44164fe9d20a2e
Jeremy Evans <code@jeremyevans.net>
2005-09-17 07:47:27 +0000

    Add index page Don't show register or reconcile account headings if no links

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,6 +8,10 @@ class Account < ActiveRecord::Base
     find(:all, :order=>'name').collect{|account|[account.scaffold_name, account.id]}
   end
   
+  def self.unhidden_register_accounts 
+    find(:all, :conditions=>"NOT hidden AND account_type IN ('Bank','CCard')", :order=>'name') 
+  end
+  
   def entries(limit=nil)
     Entry.find(:all, :include=>[:entity, :credit_account, :debit_account], :conditions=>["? IN (entries.credit_account_id, entries.debit_account_id)", id], :order=>"date DESC, (CASE WHEN reference ~ '^\\\\d{4}$' THEN reference::INTEGER ELSE 0 END) DESC, amount DESC", :limit=>limit).collect do |entry| 
       entry.main_account = self
