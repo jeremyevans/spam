d1e0f8da31b2a46f12d562ba90557eef47ca82bf
Jeremy Evans <code@jeremyevans.net>
2007-12-03 01:59:46 +0000

    Add change password feature to logged in users

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,17 +1,42 @@
 class LoginController < ApplicationController
   filter_parameter_logging :password
+  before_filter :require_login, :except=>['index', 'login']
+  
+  def update_password
+    page = 'change_password'
+    flash[:notice] = if params[:password] && params[:password2]
+      if params[:password].length < 6
+        "Password too short, use at least 6 characters, preferably 10 or more."
+      elsif params[:password] != params[:password2]
+        "Passwords don't match, please try again."
+      else
+        user = User.find(session[:user_id])
+        user.password = params[:password]
+        if user.save
+          page = 'index'
+          'Password updated.'
+        else
+          "Can't update account."
+        end
+      end
+    else
+      "No password provided, so can't change it."
+    end
+    redirect_to(:action=>page)
+  end
+  
   def login
     flash[:notice] = unless session[:user_id] = User.login_user_id(params[:username], params[:password])
-      'Incorrect username or password'
+      'Incorrect username or password.'
     else
-      'You have been logged in'
+      'You have been logged in.'
     end
     redirect_to(:action=>'index')
   end
   
   def logout
     reset_session
-    flash[:notice] = 'You have been logged out'
+    flash[:notice] = 'You have been logged out.'
     redirect_to(:action=>'index')
   end
 end
