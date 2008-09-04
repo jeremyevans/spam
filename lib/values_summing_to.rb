11af0f64910ab374dbee384183adba06d7d37710
Jeremy Evans <code@jeremyevans.net>
2008-09-04 16:34:20 -0700

    Use the man in the middle algorithm for values summing to, greatly enhancing performance

--- lib/values_summing_to.rb
+++ lib/values_summing_to.rb
@@ -1,42 +1,40 @@
 module ValuesSummingTo
+  # Return the sum of the values.
   def sum(values)
     values.inject(0){|x,y| x+=y}
   end
-
-  def combinations(array, skip = 0, &block)
-    return if (false == yield(array))
-    l = array.length
-    (l-1).downto(skip){|i| combinations(array[0...i] + array[i+1..-1], i, &block)}
+  
+  # Yield all subsets of the array to the block.
+  def subsets(array, skip = 0, &block)
+    yield(array)
+    (array.length-1).downto(skip){|i| subsets(array[0...i] + array[i+1..-1], i, &block)}
   end
 
-  def all_values_summing_to(values, want, always_block=nil, &block)
-    combinations(values.reject{|value| value == 0}) do |comb|
-      always_block.call if always_block
-      if sum(comb) == want
-        yield comb
-        next
-      end
-      pos, neg = [], []
-      comb.each{|value| ((value > 0) ? pos : neg) << value}
-      tp, tn = sum(pos), sum(neg)
-      if tp == want
-        yield pos
-        next
-      end
-      if tn == want
-        yield neg
-        next
-      end
-      unless (want > 0 && tp < want) || (want < 0 && tn >= want)
-        comb.delete_if{|value| want > 0 ? (value + tn > want) && tp -= value : (value + tp < want) && tn -= value}
+  # Yield all subsets of values that sum to want, using the meet in the
+  # middle algorithm (O(n * 2^(n/2)).
+  def all_values_summing_to(values, want, max_seconds=nil)
+    i = false
+    v1, v2 = values.partition{i=!i}
+    sums = {}
+    start_time = Time.now if max_seconds
+    subsets(v1) do |comb|
+      return if max_seconds and Time.now - start_time > max_seconds
+      sums[sum(comb)] = comb
+    end
+    subsets(v2) do |comb|
+      return if max_seconds and Time.now - start_time > max_seconds
+      if comb2 = sums[want - sum(comb)]
+        yield comb2 + comb
       end
     end
     nil
   end
 
+  # Return the first subset of values that sums to want, or nil, if no subset
+  # sums to want.
   def find_values_summing_to(values, want, max_seconds = nil)
+    # Immediately return if the value are looking for is an element of the array
     values.each{|value| return [value] if value == want}
-    start_time = Time.now if max_seconds
-    all_values_summing_to(values, want, Proc.new{return nil if max_seconds && Time.now - start_time > max_seconds}){|comb| return comb}
+    all_values_summing_to(values.reject{|value| value == 0}, want, max_seconds){|comb| return comb}
   end
 end
