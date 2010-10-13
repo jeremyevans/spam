dd5c0312584c3ffbbc3d203551f83a9c36ee5a80
Jeremy Evans <code@jeremyevans.net>
2010-10-13 11:22:37 -0700

    Fix some deprecated calls

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Sequel.extension :looser_typecasting
 
 Rails::Initializer.run do |config|
   config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-  config.action_controller.session = { :session_key => "_myapp_session", :secret => "xEc6e4EN+Pce3WYxMeIhLNaqRTLkDV5lOfs9sCd0s/HbFHYVEgHMbA=="}
+  config.action_controller.session = { :key => "_myapp_session", :secret => "xEc6e4EN+Pce3WYxMeIhLNaqRTLkDV5lOfs9sCd0s/HbFHYVEgHMbA=="}
   config.action_controller.default_charset = 'ISO-8859-1'
 end
 
