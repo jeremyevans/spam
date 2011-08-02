3591c9a944e358db728bf5db3c3cb1bd89ab4029
Jeremy Evans <code@jeremyevans.net>
2011-08-02 16:12:09 -0700

    Remove use of bundler, as there really isn't a need for it

--- Gemfile
+++
@@ -1,7 +0,0 @@
-gem 'rails', '3.0.9'
-gem 'erubis'
-gem 'sequel', :path => "/data/code/sequel"
-gem 'sequel_pg', :require=>'sequel'
-gem 'subset_sum'
-gem 'unicorn'
-gem 'scaffolding_extensions', :path => '/data/code/scaffolding_extensions'
--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 WAITTIME=5
-BUNDLE=bundle
 UNICORN=unicorn
 SPEC=spec
 if [ X"$RUBY" == "Xruby19" ]; then
-  BUNDLE=bundle19
   UNICORN=unicorn19
   SPEC=spec19
 fi
 echo -n '' > log/test.log
 echo -n '' > /var/www/logs/unicorn/spam.test.log
-$BUNDLE exec $UNICORN -c config/unicorn.test.conf -D
+$UNICORN -c config/unicorn.test.conf -D
 sleep $WAITTIME
 $SPEC test.rb
 kill `cat /var/www/tmp/spam.test.pid`
