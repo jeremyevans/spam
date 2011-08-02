d32a4505f07f681afd306ccf21ec4d537128b684
Jeremy Evans <code@jeremyevans.net>
2011-08-02 16:09:05 -0700

    Set encoding if ruby 1.9 is being used

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Spam
   class Application < Rails::Application
+    config.encoding = "ISO-8859-1" if RUBY_VERSION >= '1.9'
     config.action_controller.default_charset = 'ISO-8859-1'
     config.filter_parameters = [:password]
     config.time_zone = 'UTC'
