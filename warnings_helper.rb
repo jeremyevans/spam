5bd75d8d5e12c696467444ab58104d88a6e2afbe
Jeremy Evans <code@jeremyevans.net>
2020-06-12 15:36:56 -0700

    Only ignore certain warning types in gems

--- warnings_helper.rb
+++ warnings_helper.rb
@@ -5,6 +5,6 @@ else
   $VERBOSE = true
   Warning.ignore([:missing_ivar, :method_redefined], File.dirname(__dir__))
   Gem.path.each do |path|
-    Warning.ignore(/./, path)
+    Warning.ignore([:missing_ivar, :method_redefined, :not_reached], path)
   end
 end
